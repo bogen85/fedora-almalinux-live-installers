6fd705aed3dbd03146dfea376891c025f937528f
Dwight Schauer <dwight.schauer@teegra.net>
2023-09-12 10:58:51 -0500

    Added init-all, ls, arch, rm, and rm-all

--- bootstrap-alpine.sh
+++ bootstrap-alpine.sh
@@ -5,14 +5,31 @@ set -euo pipefail
 export BASHOPTS SHELLOPTS
 sudo=sudo
 
-function prep_root () {
+function _ls_roots () {
+  for r in /mnt/bootstraps/alpine-*-test.mnt; do
+    echo $(basename $r) | sed -e 's/^alpine-//' -e 's/-test.mnt$//'
+  done
+}
+
+function ls_roots () {
+  local _arches=$(_ls_roots)
+  [ "$_arches" == "*" ] || echo $_arches
+}
+
+function rm_root () {
   local _root_src=$1
   local _root=$2
-  if [ -d "$root" ]; then
-    $sudo rm -rf $_root/*
+  if [ -d "$_root" ]; then
     $sudo umount $_root -Rv || true
     $sudo rm -rf $_root $_root_src
   fi
+}
+
+function prep_root () {
+  local _root_src=$1
+  local _root=$2
+
+  rm_root $1 $2
 
   $sudo mkdir -pv $_root_src $_root
   $sudo mount -v -o bind $_root_src $_root
@@ -97,76 +114,118 @@ packages1=$(echo $(cat <<__END__
 __END__
 ))" "
 
-packages2=$(echo $(cat <<__END__
+__packages2__=$(echo $(cat <<__END__
 __END__
 ))" "
-
-export arch=$1
+export packages2=""
 
 arches="x64 x32 armhf ppc64le armv7 aarch64 riscv64"
-export root_src=/mnt/bootstraps/alpine-$arch-test
-export root=$root_src.mnt
 
-export alpine_arch=$arch
-export flavor=latest-stable
+function setup_arch () {
+  local _invalid_ok=$@
+  [ "$_invalid_ok" == "" ] && _invalid_ok=no
 
-case $arch in
-  armhf | armv7 | aarch64)
-    packages2+="fpc"
-    ;;
-  x64)
-    export alpine_arch=x86_64
-    packages2+="fpc"
-    ;;
-  x32)
-    export alpine_arch=x86
-    packages2+="fpc"
-    ;;
-  ppc64le)
-    packages2+="fpc-stage0"
-    ;;
-  riscv64)
-    echo WIP: $arch
-    export flavor=edge
-    ;;
-  *)
-    echo Invalid arch: $arch
-    echo Valid arches: $arches
-    exit 1
-    ;;
-esac
+  export root_src=/mnt/bootstraps/alpine-$arch-test
+  export root=$root_src.mnt
 
-shift
+  export alpine_arch=$arch
+  export flavor=latest-stable
+  export packages2=$__packages2__
+  case $arch in
+    armhf | armv7 | aarch64)
+      packages2+="fpc"
+      ;;
+    x64)
+      export alpine_arch=x86_64
+      packages2+="fpc"
+      ;;
+    x32)
+      export alpine_arch=x86
+      packages2+="fpc"
+      ;;
+    ppc64le)
+      packages2+="fpc-stage0"
+      ;;
+    riscv64)
+      echo WIP: $arch
+      export flavor=edge
+      ;;
+    *)
+      echo Invalid arch: $arch
+      echo Valid arches: $arches
+      [ "$_invalid_ok" == "no" ] && exit 1 || true
+      ;;
+  esac
 
-export packages1 packages2
-export hostname=alpine-$arch-test10
-export repos=$(cat << __END__
-https://dl-cdn.alpinelinux.org/alpine/$flavor/main
-https://dl-cdn.alpinelinux.org/alpine/$flavor/community
-https://dl-cdn.alpinelinux.org/alpine/edge/testing
+  export packages1 packages2
+  export hostname=alpine-$arch-test10
+  export repos=$(sed -e 's/ //g' << __END__
+    https://dl-cdn.alpinelinux.org/alpine/$flavor/main
+    https://dl-cdn.alpinelinux.org/alpine/$flavor/community
+    https://dl-cdn.alpinelinux.org/alpine/edge/testing
 __END__
-)
+);}
 
 args=$@
+arch="unknown"
 
 if [ "$args" == "" ]; then
   cmd="none"
 else
   cmd=$1
   shift
-  _args=$@
 fi
 
-valid_cmds="chroot init"
+invalid_ok="no"
+
+function get_arch () {
+  local _args=$@
+  if [ "$_args" == "" ]; then
+    echo Missing "<ARCH>" for $cmd
+    exit 1
+  else
+    arch=$1
+    setup_arch $invalid_ok
+  fi
+}
+
+valid_cmds="chroot init init-all ls rm rm-all arch"
 
 case "$cmd" in
+  arch)
+    echo arches: $arches
+    ;;
   chroot)
+    get_arch $@; shift; args=$@
     check_target_root
-    [ "$_args" == "" ] && _args="bash"
-    arch_chroot $root $_args
+    [ "$args" == "" ] && args="bash"
+    arch_chroot $root $args
     ;;
   init)
+    get_arch $@; shift; args=$@
     bootstrap "$packages1" "$packages2"
+    ;;
+  init-all)
+    for _arch in $arches; do
+      get_arch $_arch
+      bootstrap "$packages1" "$packages2"
+    done
+    ;;
+  ls)
+    ls_roots
+    ;;
+  rm)
+    invalid_ok=yes
+    get_arch $@; shift; args=$@
+    rm_root $root_src $root
+    ;;
+  rm-all)
+    arches=$(ls_roots)
+    invalid_ok=yes
+    for _arch in $arches; do
+      get_arch $_arch
+      rm_root $root_src $root
+    done
     ;;
   *)
     echo provided: $cmd
